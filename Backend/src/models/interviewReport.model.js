f6a6abd09eb8e744e1452eb0329c4b33044af03a
anuiscoding <anuragxgsy@gmail.com>
2026-07-25 23:57:45 +0530

    started the interviewReport schema

---
+++ Backend/src/models/interviewReport.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+
+/**
+ * Job description schema
+ * resume text
+ * Self description
+ * 
+ * -technical question
+ * -behavioural question
+ * -skill gaps
+ * -preparation plan :[{}]
+*/
