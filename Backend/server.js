64da9899e840474efeb20aa061a8d996ebd2e72b
anuiscoding <anuragxgsy@gmail.com>
2026-07-26 23:51:27 +0530

    AI API set up and result schema

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,14 @@
 const dns = require("dns");
-
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 require("dotenv").config();
-
 const app = require("./src/app");
 const connectDB = require("./src/config/database");
+const invokeGeminiAi = require("./src/services/ai.service")
+
 
 connectDB();
+invokeGeminiAi()
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
