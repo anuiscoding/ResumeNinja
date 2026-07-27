324c152b076c9a8305a9388250d6d1e8c31bce71
anuiscoding <anuragxgsy@gmail.com>
2026-07-27 23:59:21 +0530

    commit

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,4 +1,6 @@
 const { GoogleGenAI } = require("@google/genai")
+const { z } = require("zod")
+const { zodToJsonSchema } = require("zod-to-json-schema")
 
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_GENAI_API_KEY
